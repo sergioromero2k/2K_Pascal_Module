f05840f4f00ea303dad868dd2d9de5e050e47346
Sergio Alejandro <sergioromero2k@gmail.com>
2026-09-12 15:39:09 +0200

    feat(module-0): add Saiyan power exercise

--- modules/module_0/ex0/saiyan_power.pas
+++ modules/module_0/ex0/saiyan_power.pas
@@ -1,10 +1,7 @@
-{
-  File: saiyan_power.pas
-  Author: Sergio
-  Description: Module 00 - ex00: Display Prince of Saiyans greeting.
-}
-program SaiyanPower;
 
-begin
+Program SaiyanPower;
+
+Begin
+  { Display the initial training message }
   writeln('I am the Prince of all Saiyans! Training starts now.');
-end.
+End.
